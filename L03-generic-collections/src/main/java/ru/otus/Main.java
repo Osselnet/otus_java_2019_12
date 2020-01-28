0f582dd55c08c4d2e80949884d59c862cb4f979d
Osselnet <hornets@list.ru>
2020-01-28 09:38:29 +0300

    added code that checks if the size property changes correctly

--- L03-generic-collections/src/main/java/ru/otus/Main.java
+++ L03-generic-collections/src/main/java/ru/otus/Main.java
@@ -1,29 +1,23 @@
 package ru.otus;
+
 import ru.otus.DIYarrayList;
+
 import java.util.Collections;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         DIYarrayList<String> dList1 = new DIYarrayList<String>();
-        DIYarrayList<String> dList2 = new DIYarrayList<String>(50);
-
-        for (int i = 51; i <= 100; i++) {
-            dList2.add("");
-            if (dList2.size() != i) {
-                System.err.println("Illegal size: " + dList2.size());
-                break;
-            }
-        }
+        DIYarrayList<String> dList2 = new DIYarrayList<String>(100);
 
         String[] strings = new String[100];
-        for (int i = 0; i<100; i++){
+        for (int i = 0; i < 100; i++) {
             strings[i] = Integer.toString(new Random().nextInt(100));
+            dList2.add("");
         }
 
         Collections.addAll(dList1, strings);
         Collections.copy(dList2, dList1);
         Collections.sort(dList2, null);
-
     }
 }
