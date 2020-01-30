ed3683728564f4d5138c442b7e1875cf40e94476
Osselnet <hornets@list.ru>
2020-01-30 14:54:30 +0300

    created a module to find out the difference between different gc.

---
+++ L06-gc/src/main/java/ru/otus/Main.java
@@ -0,0 +1,28 @@
+package ru.otus;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+// -Xms256m
+// -Xmx256m
+// -Xloggc:./hw05_GC/logs/SerialGC.log
+// -XX:+HeapDumpOnOutOfMemoryError
+// -XX:HeapDumpPath=./logs/dump
+
+// -XX:+UseSerialGC
+// -XX:+UseParallelGC
+// -XX:+UseG1GC
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ru.otus:type=Benchmark");
+        Benchmark mbean = new Benchmark();
+        mbs.registerMBean(mbean, name);
+        int size = 2000;
+        mbean.setSize(size);
+        mbean.run();
+    }
+}
